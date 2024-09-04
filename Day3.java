a41fe27ee4fedb3178d94321a81705cbe9547aa5
A. Irgi Irwan. A <irgiunsulbar24@gmail.com>
2024-09-04 16:14:20 +0800

    Day3.java
    Menambahkan System.out.println

--- Day3.java
+++ Day3.java
@@ -2,6 +2,8 @@ package SeratusHariJava;
 
 public class Day3 {
     public static void main(String[] args) {
-        System.out.println("Assalamu'alaikum wr. wb. Halo Semuanya!");
+        System.out.println("Nama    : A. Irgi Irwan. A");
+        System.out.println("NIM     : D0224318");
+        System.out.println("Alamat  : Dusun Puppole");
     }
 }
