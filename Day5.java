2ea4e2e0a29c4128bc1e2351acb7d2d931c297e7
A. Irgi Irwan. A <irgiunsulbar24@gmail.com>
2024-09-06 11:04:24 +0800

    Create Day5.java
    Contoh Penambahkan komentar di java

---
+++ Day5.java
@@ -0,0 +1,17 @@
+package SeratusHariJava;
+
+public class Day5 {
+    public static void main(String[] args) {
+    //        Ini adalah Komentar 1 Baris
+
+    /* Ini adalah Komentar 1 Paragraf
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui molestiae quaerat ipsam eligendi quibusdam atque ea,
+    doloribus facilis, quae accusamus officia libero optio pariatur quam perspiciatis? Perspiciatis vero aperiam nostrum
+    nihil sit nisi qui laudantium deleniti, mollitia quod, inventore quas nesciunt accusamus porro, rerum labore quaerat
+    voluptatum? Pariatur reiciendis mollitia, voluptatibus qui labore vitae fugiat odit tenetur obcaecati tempore,
+    repellendus corrupti dolorem voluptas vero eligendi explicabo consequuntur repudiandae doloremque? Consectetur non
+    ea nihil delectus aut doloribus facere pariatur, aliquid architecto porro quos, amet provident a laboriosam deleniti
+    labore odit asperiores excepturi repudiandae reiciendis. Laudantium sed officiis veniam, officia suscipit a. */
+
+    }
+}
