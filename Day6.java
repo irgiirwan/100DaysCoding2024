d9d63e4fa3c684c2bb103889ff35d22cf253aa52
A. Irgi Irwan. A <irgiunsulbar24@gmail.com>
2024-09-07 15:59:50 +0800

    Day6.java
    Membuatan Variabel Deklarasi di Java.

---
+++ Day6.java
@@ -0,0 +1,23 @@
+package SeratusHariJava;
+
+public class Day6 {
+    public static void main(String[] args) {
+        // Tugas Pembuatan Variabel Deklarasi di Java.
+        
+        // Cara Pertama
+//        String nama;
+//        String nim;
+//        String alamat;
+        
+        // Cara Kedua
+        String nama, nim, alamat;
+        
+        nama = "A. Irgi Irwan. A";
+        nim = "D0224318";
+        alamat = "Dusun Puppole";
+        
+        System.out.println("Nama   : "+nama);
+        System.out.println("NIM    : "+nim);
+        System.out.println("Alamat : "+alamat);
+    }
+}
