fee6ab3a2d66ab405bb30c70459069ee4dc2da0a
A. Irgi Irwan. A <irgiunsulbar24@gmail.com>
2024-09-08 13:43:58 +0800

    Day7.java
    Membuatan Variabel Inisialisasi di java

---
+++ Day7.java
@@ -0,0 +1,27 @@
+package SeratusHariJava;
+
+public class Day7 {
+
+    public static void main(String[] args) {
+        // Tugas Pembuatan Variabel Inisialisasi di java
+
+        // Cara Pertama
+        /*
+        String nama, nim, alamat;
+        
+        nama = "A. Irgi Irwan. A";
+        nim = "D0224318";
+        alamat = "Dusun Puppole";
+        */
+        
+        // Cara Kedua
+        String nama = "A. Irgi Irwan.  A";
+        String nim = "D0224318";
+        String alamat = "Dusun Puppole";
+        
+        // ====================================
+        System.out.println("Nama   : "+nama);
+        System.out.println("NIM    : "+nim);
+        System.out.println("Alamat : "+alamat);
+    }
+}
