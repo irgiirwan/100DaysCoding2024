1023cfc490faf1400459a22395f3f967c1323b03
A. Irgi Irwan. A <irgiunsulbar24@gmail.com>
2024-09-09 12:12:41 +0800

    Day8.java
    Tugas Pembuatan Variabel Update

---
+++ Day8.java
@@ -0,0 +1,31 @@
+package SeratusHariJava;
+
+public class Day8 {
+    public static void main(String[] args) {
+        // Tugas Pembuatan Variabel Update
+        
+        // Memasukan nilai awal variabel
+        String nama = "A. Irgi Irwan. A";
+        String pesan = "Halo! Apa Kabar";
+        String lokasi = "Kec Campalagian";
+        
+        // Menampilkan nilai awal variiabel
+        System.out.println("Nama         : "+nama);
+        System.out.println("Pesan        : "+pesan);
+        System.out.println("Lokasi Pesan : "+lokasi);
+        
+        System.out.println("");
+        System.out.println("=========================");
+        System.out.println("");
+        
+        // Mengubah(Update) nilai variabel
+        nama = "irgi";
+        pesan = "Tunjukkan! Ku ingin bertemu";
+        lokasi = "Kab Majene";
+        
+        // Menampilkan update nilai variiabel
+        System.out.println("Nama         : "+nama);
+        System.out.println("Pesan        : "+pesan);
+        System.out.println("Lokasi Pesan : "+lokasi);
+    }
+}
