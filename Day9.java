fb5c6998dc3f190875fe269b67b596dd6a214208
A. Irgi Irwan. A <irgiunsulbar24@gmail.com>
2024-09-10 20:06:00 +0800

    Day9.java
    Membuatan Variabel Constanta (Final Variable)

---
+++ Day9.java
@@ -0,0 +1,19 @@
+package SeratusHariJava;
+
+public class Day9 {
+    public static final String nama  = "A. Irgi Irwan. A";
+    public static final String pesan = "Kenapa kau bersembunyi?";
+    public static final String alamat = "Kec Campalagian";
+
+    public static void main(String[] args) {
+        // Tugas Pembuatan Variabel Constanta (Final Variable)
+        
+        System.out.println("Nama         : "+nama);
+        System.out.println("Pesan        : "+pesan);
+        System.out.println("Alamat Pesan : "+alamat);
+        
+        // Akan error jika ingin update nilai awal variabel karena sudah tidak bisa. contohnya nama variabelnya nama
+        // nama = "irgi";
+        
+    }
+}
