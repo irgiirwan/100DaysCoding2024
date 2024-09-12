76a909d5ed1edbbf2fe8cf3b90bcd6676465a274
A. Irgi Irwan. A <irgiunsulbar24@gmail.com>
2024-09-12 19:11:28 +0800

    Day11.java
    Menggunaan tipe data short

---
+++ Day11.java
@@ -0,0 +1,14 @@
+package SeratusHariJava;
+
+public class Day11 {
+    public static void main(String[] args) {
+        // Tugas penggunaan tipe data short
+        
+        short nilai1 = -32, nilai2 = 32;
+        
+        System.out.println("Jadi nilai pertama adalah "+nilai1);
+        System.out.println("Jadi nilai kedua adalah "+nilai2);
+        
+        // jika melebihi nilai min dan max nya maka akan menampilkan pesan error, terima kasih :)
+    }
+}
