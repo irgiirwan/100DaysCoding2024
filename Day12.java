2ab074f61d31a2e7c285a98d48a42a057b16fab2
A. Irgi Irwan. A <irgiunsulbar24@gmail.com>
2024-09-13 10:25:10 +0800

    Day12.java
    Menggunaan tipe data int

---
+++ Day12.java
@@ -0,0 +1,14 @@
+package SeratusHariJava;
+
+public class Day12 {
+    public static void main(String[] args) {
+        // Tugas penggunaan tipe data int
+        
+        int nilai1 = 16, nilai2 = -16;
+        
+        System.out.println("Jadi nilai pertama adalah "+nilai1);
+        System.out.println("Jadi nilai kedua adalah "+nilai2);
+        
+        // Jika melebihi nilai min dan max maka akan menampilkan pesan error, sekian terima gaji(kasih) :)
+    }
+}
