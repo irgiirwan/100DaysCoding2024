5f4785810a2fa540c75abee253360f6953b823d9
A. Irgi Irwan. A <irgiunsulbar24@gmail.com>
2024-09-14 19:28:21 +0800

    Day13.java
    Ujian Evaluasi

---
+++ Day13.java
@@ -0,0 +1,27 @@
+package SeratusHariJava;
+
+public class Day13 {
+
+    public static void main(String[] args) {
+        /*
+        Soal Evaluasi :
+        1. Buat project
+        2. Buat tiga jenis tipe data (byte,short, dan int)
+        3. buat masing² variabelnya
+        4. isi nilai masing² variabel
+        5. Panggil menggunakan System.out.prinln (buat tiga baris)
+         */
+
+        byte nilai1;
+        short nilai2;
+        int nilai3;
+
+        nilai1 = 8;
+        nilai2 = 16;
+        nilai3 = 32;
+
+        System.out.println("Isi nilai pertama adalah " + nilai1);
+        System.out.println("Isi nilai kedua adalah " + nilai2);
+        System.out.println("Isi nilai ketiga adalah " + nilai3);
+    }
+}
