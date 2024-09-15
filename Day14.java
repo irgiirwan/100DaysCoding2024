6054a78d566a040b88439434c64de96ce8e6be71
A. Irgi Irwan. A <irgiunsulbar24@gmail.com>
2024-09-15 22:48:23 +0800

    Day14.java
    Menggunakan tipe data long

---
+++ Day14.java
@@ -0,0 +1,18 @@
+package SeratusHariJava;
+
+public class Day14 {
+    public static void main(String[] args) {
+        // Tugas Penggunaan tipe data long
+        
+        long nilai1 = -9876543210L;
+        long nilai2 = 9876543210L;
+        
+        /*
+        Menambah L di akhir angka yang menandakan bahwa ini menggunakan tipe data long,
+        jika tidak menambahkan L maka di baca sebagai tipe data int.
+        */
+        
+        System.out.println("Jadi isi nilai pertama adalah "+nilai1);
+        System.out.println("Jadi isi nilai kedua adalah "+nilai2);
+    }
+}
