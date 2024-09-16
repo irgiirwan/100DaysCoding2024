3e947c3fc3676c049225e03302d42c27a3137b2a
A. Irgi Irwan. A <irgiunsulbar24@gmail.com>
2024-09-16 23:36:35 +0800

    Day15.java
    Membuatan variabel dengan tipe data double

---
+++ Day15.java
@@ -0,0 +1,13 @@
+package SeratusHariJava;
+
+public class Day15 {
+    public static void main(String[] args) {
+        // Tugas Pembuatan dengan tipe data double
+        
+        double nilai1 = 3.14;
+        double nilai2 = -3.14;
+        
+        System.out.println("Jadi nilai pertama adalah "+nilai1);
+        System.out.println("Jadi nilai kedua adalah "+nilai2);
+    }
+}
