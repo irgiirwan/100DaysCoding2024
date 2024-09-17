67668b3ac0c6433bffa18d45ad2c2549873da7a1
A. Irgi Irwan. A <irgiunsulbar24@gmail.com>
2024-09-17 12:01:28 +0800

    Day16.java
    Membuat dengan tipe data float

---
+++ Day16.java
@@ -0,0 +1,13 @@
+package SeratusHariJava;
+
+public class Day16 {
+    public static void main(String[] args) {
+        // Tugas Pembuatan dengan tipe data float
+        
+        float nilai1 = 3.14f;
+        float nilai2 = -3.14f;
+        
+        System.out.println("Jadi nilai pertama adalah "+nilai1);
+        System.out.println("Jadi nilai kedua adalah "+nilai2);
+    }
+}
