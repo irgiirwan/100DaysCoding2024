7b105bb30623e0238367c50c340f84fbe483234b
A. Irgi Irwan. A <irgiunsulbar24@gmail.com>
2024-09-18 23:15:10 +0800

    Day17.java
    Membuat dengan menggunakan tipe data char

---
+++ Day17.java
@@ -0,0 +1,11 @@
+package SeratusHariJava;
+
+public class Day17 {
+    public static void main(String[] args) {
+        // tugas pembuatan dengan tipe data char
+        
+        char kelas = 'C';
+        
+        System.out.println("Kelas: 2024/"+kelas);
+    }
+}
