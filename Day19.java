7af4db81a18dd54cc2256f179da5a25c9ee9688c
A. Irgi Irwan. A <irgiunsulbar24@gmail.com>
2024-09-20 23:42:49 +0800

    Day19.java
    Tugas pembuatan tipe data reference string

---
+++ Day19.java
@@ -0,0 +1,15 @@
+package SeratusHariJava;
+
+public class Day19 {
+    
+    //Tugas pembuatan tipe data reference string
+
+    String nama;
+
+    public static void main(String[] args) {
+        Day19 indentitas = new Day19();
+        indentitas.nama = "A. Irgi Irwan. A";
+        System.out.println("Nama Anda: " + indentitas.nama);
+
+    }
+}
