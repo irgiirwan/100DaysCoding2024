b2f743ea2cbc3fe9b710aefc575f4172cb558a43
A. Irgi Irwan. A <irgiunsulbar24@gmail.com>
2024-09-21 23:09:09 +0800

    Day20.java
    Tugas penggunaan Tipe data reference Byte

---
+++ Day20.java
@@ -0,0 +1,11 @@
+package SeratusHariJava;
+
+public class Day20 {
+    public static void main(String[] args) {
+        // Tugas penggunaan Tipe data reference Byte
+        
+        Byte objekByte = 10;  // Membuat objek Byte dengan nilai 10
+        
+        System.out.println("Nilai Objek Byte: " + objekByte);  // Output: 10
+    }
+}
