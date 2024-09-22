1e8326861c8a52c0dbf6e501bdda2ec637eef09c
A. Irgi Irwan. A <irgiunsulbar24@gmail.com>
2024-09-22 23:31:35 +0800

    Day21.java
    Tugas Penggunaan tipe data reference Short

---
+++ Day21.java
@@ -0,0 +1,11 @@
+package SeratusHariJava;
+
+public class Day21 {
+    public static void main(String[] args) {
+        //Tugas Penggunaan tipe data reference Short
+        
+        Short objekShort = 12; // membuat objek Short dengan nilai 12
+        
+        System.out.println("Nilai objek Short: "+objekShort);
+    }
+}
