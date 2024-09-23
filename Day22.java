f6f8c5fa843a00aa35c543ced6fdb3de560bbcb4
A. Irgi Irwan. A <irgiunsulbar24@gmail.com>
2024-09-23 23:10:52 +0800

    Day22.java
    Tugas Penggunaan tipe data reference Integer

---
+++ Day22.java
@@ -0,0 +1,11 @@
+package SeratusHariJava;
+
+public class Day22 {
+    public static void main(String[] args) {
+        //Tugas Penggunaan tipe data reference Integer
+        
+        Integer objekInteger = 32; // membuat objek Integer dengan nilai 32
+        
+        System.out.println("Nilai objek Integer: "+objekInteger);
+    }
+}
