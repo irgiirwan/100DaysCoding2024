06539f446b47e8af4ecf9c62a4e130356bae5f76
A. Irgi Irwan. A <irgiunsulbar24@gmail.com>
2024-09-24 23:38:00 +0800

    Day23.java
    Tugas penggunaan tipe data reference Long

---
+++ Day23.java
@@ -0,0 +1,11 @@
+package SeratusHariJava;
+
+public class Day23 {
+    public static void main(String[] args) {
+        //Tugas penggunaan tipe data reference Long
+        
+        Long objekLong = 64L; // membuat objek long dengan nilai 64
+        
+        System.out.println("Nilai Objek Long: "+objekLong);
+    }
+}
