e2de57730d73c0c1fb139154e8ee5297ba505745
A. Irgi Irwan. A <irgiunsulbar24@gmail.com>
2024-09-26 02:58:26 +0800

    Update Day24.java

--- Day24.java
+++ Day24.java
@@ -4,8 +4,8 @@ public class Day24 {
     public static void main(String[] args) {
         //Tugas penggunaan tipe data reference Float
         
-        Float objekFloat = 3.14F;
+        Float objekFloat = 3.14F; //memiliki nilai
         
-        System.out.println("Nilai Objek Float: "+objekFloat);
+        System.out.println("Nilai Objek Float: "+objekFloat); // perintah menampilkan
     }
 }
