709fa25394a0feea39ed6cf4ef52b4dcd727d421
A. Irgi Irwan. A <irgiunsulbar24@gmail.com>
2024-09-26 23:19:44 +0800

    Day25.java

---
+++ Day25.java
@@ -0,0 +1,11 @@
+package SeratusHariJava;
+
+public class Day25 {
+    public static void main(String[] args) {
+        //Tugas penggunaan tipe data reference Double
+        
+        Double objekDouble = 3.14;
+        
+        System.out.println("Nilai Objek Double: "+objekDouble);
+    }
+}  
