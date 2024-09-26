4cf03bf86c2d0c73a342c838aa6db524bc693e05
A. Irgi Irwan. A <irgiunsulbar24@gmail.com>
2024-09-27 05:20:04 +0800

    Day26.java

---
+++ Day26.java
@@ -0,0 +1,11 @@
+package SeratusHariJava;
+
+public class Day26 {
+    public static void main(String[] args) {
+        //Tugas penggunaan tipe data reference Character
+        
+        Character objekCharacter = 'A';
+        
+        System.out.println("Isi Objek Character: "+objekCharacter);
+    }
+}  
