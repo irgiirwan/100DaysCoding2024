afbb04c311d70279a5a0e62f3b07a2ea8255b495
A. Irgi Irwan. A <irgiunsulbar24@gmail.com>
2024-09-29 23:21:30 +0800

    Day28.java
    Tugas penggunaan tipe data reference Boolean

---
+++ Day28.java
@@ -0,0 +1,11 @@
+package SeratusHariJava;
+
+public class Day28 {
+    public static void main(String[] args) {
+        //Tugas penggunaan tipe data reference Boolean
+        
+        Boolean objekBoolean = true;
+        
+        System.out.println("Ah yang Benar? \nJawaban: "+objekBoolean);
+    }
+}
