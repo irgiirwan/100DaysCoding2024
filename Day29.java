60ac09ce033da1a7ff641ff125ae37f4bc573619
A. Irgi Irwan. A <irgiunsulbar24@gmail.com>
2024-09-30 06:01:40 +0800

    Day29.java
    penggunaan tipe data reference BigInteger

---
+++ Day29.java
@@ -0,0 +1,13 @@
+package SeratusHariJava;
+
+import java.math.BigInteger;
+
+public class Day29 {
+    public static void main(String[] args) {
+        //Tugas penggunaan tipe data reference BigInteger
+        
+        BigInteger objekBig = new BigInteger("0123456789012345678901234567890123456789");
+        
+        System.out.println("nilai objek Big Integer: "+objekBig);
+    }
+}
