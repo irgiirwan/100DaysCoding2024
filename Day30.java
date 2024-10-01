4c56adfdb052a6f030e3b340ebbf60ee230f0118
A. Irgi Irwan. A <irgiunsulbar24@gmail.com>
2024-10-01 21:47:17 +0800

    Day30.java

---
+++ Day30.java
@@ -0,0 +1,13 @@
+package SeratusHariJava;
+
+import java.math.BigDecimal;
+
+public class Day30 {
+    public static void main(String[] args) {
+        //Tugas penggunaan tipe data reference BigDecimal
+        
+        BigDecimal objekBig = new BigDecimal("01234567890123456789.01234567890123456789");
+        
+        System.out.println("nilai objek Big Integer: "+objekBig);
+    }
+}
