2acd46f293d76bf7b86359ed42bb8f138fcd6e9d
A. Irgi Irwan. A <irgiunsulbar24@gmail.com>
2024-10-03 22:26:42 +0800

    Day32.java

---
+++ Day32.java
@@ -0,0 +1,12 @@
+package SeratusHariJava;
+
+public class Day32 {
+    public static void main(String[] args) {
+        //Tugas Konversi Tipe Data Primitif String
+
+        double AngDbl = 2826.0673;
+        String AngStr = String.valueOf(AngDbl);
+
+        System.out.println("nilainya adalah: "+AngStr);
+    }
+}
