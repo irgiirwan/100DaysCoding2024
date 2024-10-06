1a206352efaa1d12cd535b9000344fdca2b21a8a
A. Irgi Irwan. A <irgiunsulbar24@gmail.com>
2024-10-06 21:57:15 +0800

    Day35.java
    Tugas Operator Aritmatika : Penjumlahan & Pengurangan

---
+++ Day35.java
@@ -0,0 +1,16 @@
+package SeratusHariJava;
+
+public class Day35 {
+
+    public static void main(String[] args) {
+        //Tugas Operator Aritmatika : Penjumlahan & Pengurangan
+        
+        int a = 28, b = 26;
+        
+        // Penjumlahan
+        System.out.println("Hasil dari "+a+" + "+b+" = "+(a+b));
+        
+        // Pengurangan
+        System.out.println("Hasil dari "+a+" - "+b+" = "+(a-b));
+    }
+}
