41923e7948b22fe20d69c83a5e8a86d998cdacc5
A. Irgi Irwan. A <irgiunsulbar24@gmail.com>
2024-10-07 23:08:38 +0800

    Day36.java
    Tugas Operator Aritmatika : Perkalian, Pembagian serta Modulo

---
+++ Day36.java
@@ -0,0 +1,23 @@
+package SeratusHariJava;
+
+public class Day36 {
+
+    public static void main(String[] args) {
+        //Tugas Operator Aritmatika : Perkalian, Pembagian serta Modulo
+        
+        int a = 28, b = 14;
+        
+        int Perkalian = a*b;
+        double Pembagian = a/b;
+        int Modulo = a%b;
+        
+        // Perkalian
+        System.out.println("Hasil dari "+a+" x "+b+" = "+Perkalian);
+        
+        // Pembagian
+        System.out.println("Hasil dari "+a+" / "+b+" = "+Pembagian);
+        
+        // Modulo
+        System.out.println("Hasil dari "+a+" % "+b+" = "+Modulo);
+    }
+}
