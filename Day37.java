a38f09569fd94ef95d94a5e871a17ac9a32ba1e6
A. Irgi Irwan. A <irgiunsulbar24@gmail.com>
2024-10-08 23:08:47 +0800

    Day37.java
    Tugas Operator Penugasan : Pengisian Nilai, Pengisian dan Penambahan, Pengisian dan Pengurangan

---
+++ Day37.java
@@ -0,0 +1,22 @@
+package SeratusHariJava;
+
+public class Day37 {
+
+    public static void main(String[] args) {
+        //Tugas Operator Penugasan : Pengisian Nilai, Pengisian dan Penambahan, Pengisian dan Pengurangan
+        
+        int tempe = 9; // Pengisian Nilai
+        
+        System.out.println("Tempe saat ini berjumlah : "+tempe);
+        
+        tempe += 3; // Pengisian dan Penambahan
+        System.out.println("");
+        System.out.println("Proses penambahan tempe");
+        System.out.println("Jadi tempe saat ini setelah penambahan berjumlah : "+tempe);
+        
+        tempe -= 2; // Pengisian dan Pengurangan
+        System.out.println("");
+        System.out.println("Proses pengurangan tempe");
+        System.out.println("Jadi tempe saat ini setelah pengurangan berjumlah : "+tempe);
+    }
+}
