cbda25772ca1af3d056b65028f5ff02795e52c46
A. Irgi Irwan. A <irgiunsulbar24@gmail.com>
2024-10-09 22:24:30 +0800

    Day38.java
    Tugas Operator Penugasan : Pengisian dan Perkalian, Pengisian dan Pembagia

---
+++ Day38.java
@@ -0,0 +1,22 @@
+package SeratusHariJava;
+
+public class Day38 {
+
+    public static void main(String[] args) {
+        //Tugas Operator Penugasan : Pengisian dan Perkalian, Pengisian dan Pembagian
+        
+        int nilai = 9; // Pengisian Nilai
+        
+        System.out.println("Hasil Pengisian : "+nilai);
+        
+        nilai *= 4; // Pengisian dan perkalian
+        System.out.println("");
+        System.out.println("Proses perkalian nilai");
+        System.out.println("Hasil pengisian dan perkalian : "+nilai);
+        
+        nilai /= 2; // Pengisian dan pembagian
+        System.out.println("");
+        System.out.println("Proses pembagian nilai");
+        System.out.println("Hasil pengisian dan pembagian : "+nilai);
+    }
+}
