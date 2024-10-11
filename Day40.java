c1c6c748e6fd5968635afbd1a9a91749af5e90f0
A. Irgi Irwan. A <irgiunsulbar24@gmail.com>
2024-10-11 22:08:17 +0800

    Day40.java
    Tugas Operator Perbandingan : Lebih besar dan lebih kecil

---
+++ Day40.java
@@ -0,0 +1,13 @@
+package SeratusHariJava;
+
+public class Day40 {
+
+    public static void main(String[] args) {
+        //Tugas Operator Perbandingan : Lebih besar dan lebih kecil
+        
+        int a = 5, b = 10;
+        
+        System.out.println("Apakah "+a+" > "+b+" : "+(a>b));
+        System.out.println("Apakah "+a+" < "+b+" : "+(a<b));
+    }
+}
