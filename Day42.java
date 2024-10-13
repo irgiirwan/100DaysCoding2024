ee18a6cb4803362731c35d5540dd04f9db70e10a
A. Irgi Irwan. A <irgiunsulbar24@gmail.com>
2024-10-13 22:43:56 +0800

    Day42.java
    Tugas Operator Perbandingan : Sama dengan dan tidak sama dengan

---
+++ Day42.java
@@ -0,0 +1,13 @@
+package SeratusHariJava;
+
+public class Day42 {
+
+    public static void main(String[] args) {
+        //Tugas Operator Perbandingan : Sama dengan dan tidak sama dengan
+        
+        int a = 5, b = 10;
+        
+        System.out.println("Apakah "+a+" == "+b+" : "+(a==b));
+        System.out.println("Apakah "+a+" != "+b+" : "+(a!=b));
+    }
+}
