efa9fdcbe7375dde709471c7997de6d42dda6ff1
A. Irgi Irwan. A <irgiunsulbar24@gmail.com>
2024-10-14 22:31:29 +0800

    Day43.java
    Operator Perbandingan : Lebih besar sama dengan dan lebih kecil sama dengan

---
+++ Day43.java
@@ -0,0 +1,14 @@
+package SeratusHariJava;
+
+public class Day43 {
+
+    public static void main(String[] args) {
+        //Operator Perbandingan : Lebih besar sama dengan dan lebih kecil sama dengan
+        
+        int a = 5, b = 10, c = 10;
+        
+        System.out.println("Apakah "+b+" >= "+c+" : "+(b>=c)+" dan apakah "+b+" <= "+c+" : "+(b<=c));
+        System.out.println("Apakah "+a+" >= "+b+" : "+(a>=b));
+        System.out.println("Apakah "+a+" <= "+b+" : "+(a<=b));
+    }
+}
