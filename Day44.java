63d785c8545bf85926b4300cdb84a44fd5d96d70
A. Irgi Irwan. A <irgiunsulbar24@gmail.com>
2024-10-15 23:21:31 +0800

    Day44.java

---
+++ Day44.java
@@ -0,0 +1,15 @@
+package SeratusHariJava;
+
+public class Day44 {
+
+    public static void main(String[] args) {
+        //Operator Logika : AND
+        
+        boolean a = false, b = true, c = false, d = true;
+        
+        System.out.println(b && d);
+        System.out.println(a && b);
+        System.out.println(b && c);
+        System.out.println(a && c);
+    }
+}
