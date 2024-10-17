67a4a661d75ffd1c289292ae28b58819c1d516c6
A. Irgi Irwan. A <irgiunsulbar24@gmail.com>
2024-10-17 23:16:14 +0800

    Day46.java
    Operator Logika : Negasi/kebalikan

---
+++ Day46.java
@@ -0,0 +1,15 @@
+package SeratusHariJava;
+
+public class Day46 {
+
+    public static void main(String[] args) {
+        //Operator Logika : Negasi/kebalikan
+        
+        boolean a = false, b = true;
+        
+        System.out.println(a);
+        System.out.println(!a);
+        System.out.println(b);
+        System.out.println(!b);
+    }
+}
