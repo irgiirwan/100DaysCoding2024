0e3e1e31b29fb9b4f92de5e6816889261751d8d9
A. Irgi Irwan. A <irgiunsulbar24@gmail.com>
2024-10-20 23:49:17 +0800

    Day49.java
    Percabangan : if-else if

---
+++ Day49.java
@@ -0,0 +1,16 @@
+package SeratusHariJava;
+
+public class Day49 {
+
+    public static void main(String[] args) {
+        //Percabangan : if-else if
+
+        int angka1 = -100;
+
+        if (angka1 > 0) {
+            System.out.println("Angka Positif");
+        } else if(angka1 < 0) {
+            System.out.println("Angka Negatif");
+        }
+    }
+}
