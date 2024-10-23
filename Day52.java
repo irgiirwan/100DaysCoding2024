60e295ea696ecff12496d71d2e35047a191306c0
A. Irgi Irwan. A <irgiunsulbar24@gmail.com>
2024-10-23 22:18:39 +0800

    Day52.java
    Ternary

---
+++ Day52.java
@@ -0,0 +1,12 @@
+package SeratusHariJava;
+
+public class Day52 {
+
+    public static void main(String[] args) {
+        //Ternary
+
+        int umur = 17;
+
+        String ket = (umur >= 18) ? "Dewasa" : "Belum Dewasa";
+    }
+}
