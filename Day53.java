fdc39aab74470f823dedc8a34c4ed6ba3cf04b9f
A. Irgi Irwan. A <irgiunsulbar24@gmail.com>
2024-10-24 23:20:52 +0800

    Day53.java
    Looping : for

---
+++ Day53.java
@@ -0,0 +1,12 @@
+package SeratusHariJava;
+
+public class Day53 {
+
+    public static void main(String[] args) {
+        //Looping : for
+
+        for(int i = 1; i <= 5; i++) {
+            System.out.println("Testing dicoba");
+        }
+    }
+}
