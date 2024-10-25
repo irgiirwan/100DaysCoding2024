def529d49894b7a25d5996c78621d8b67fddf296
A. Irgi Irwan. A <irgiunsulbar24@gmail.com>
2024-10-25 23:10:18 +0800

    Day54.java
    Looping : for Increment

---
+++ Day54.java
@@ -0,0 +1,12 @@
+package SeratusHariJava;
+
+public class Day54 {
+
+    public static void main(String[] args) {
+        //Looping : for Increment
+
+        for(int i = 1; i <= 10; i++) {
+            System.out.println("Angka Ke-"+i);
+        }
+    }
+}
