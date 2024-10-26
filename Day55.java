85d47b06d1adf8df9c9f4a93a4f070f784b2059e
A. Irgi Irwan. A <irgiunsulbar24@gmail.com>
2024-10-26 23:39:20 +0800

    Day55.java
    Looping : for Decrement

---
+++ Day55.java
@@ -0,0 +1,12 @@
+package SeratusHariJava;
+
+public class Day55 {
+
+    public static void main(String[] args) {
+        //Looping : for Decrement
+
+        for(int i = 10; i >= 1; i--) {
+            System.out.println("Angka Ke-"+i);
+        }
+    }
+}
