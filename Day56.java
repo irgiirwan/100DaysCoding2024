f48fff4d9e38b0c87b1731296bbe4127db06d9fa
A. Irgi Irwan. A <irgiunsulbar24@gmail.com>
2024-10-28 22:50:57 +0800

    Day56.java
    While Increment

---
+++ Day56.java
@@ -0,0 +1,15 @@
+package SeratusHariJava;
+
+public class Day56 {
+
+    public static void main(String[] args) {
+        //Looping : While Increment
+
+        int i = 1;
+        
+        while(i <= 10) {
+            System.out.println("Angka ke-"+i);
+            i++;
+        }
+    }
+}
