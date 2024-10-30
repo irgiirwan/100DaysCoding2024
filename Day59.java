ec6721fc2e5de6ff30aad0565e325afa02978d37
A. Irgi Irwan. A <irgiunsulbar24@gmail.com>
2024-10-30 22:11:39 +0800

    Day59.java
    For Each

---
+++ Day59.java
@@ -0,0 +1,13 @@
+package SeratusHariJava;
+
+public class Day59 {
+
+    public static void main(String[] args) {
+        //For Each 
+
+        String warga[] = {"Adit","Jack","Anna","Siti","Jamal"};
+        for(String orang : warga) {
+            System.out.println("Nama Warga: "+orang);
+        }
+    }
+}
