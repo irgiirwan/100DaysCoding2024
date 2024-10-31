5b43991b387799a897bb829ff4d667dabce173a2
A. Irgi Irwan. A <irgiunsulbar24@gmail.com>
2024-10-31 21:19:49 +0800

    Day60.java
    do while

---
+++ Day60.java
@@ -0,0 +1,15 @@
+package SeratusHariJava;
+
+public class Day60 {
+
+    public static void main(String[] args) {
+        // do while
+        
+        int i = 1;
+        
+        do {
+            System.out.println("Angka ke-"+i);
+            i++;
+        }while(i <= 10);
+    }
+}
