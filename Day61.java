3d6f1d2e7ab5935ab3772d58bc28b53d4cca1f49
A. Irgi Irwan. A <irgiunsulbar24@gmail.com>
2024-11-01 23:14:51 +0800

    Day61.java
    Infinite Loop

---
+++ Day61.java
@@ -0,0 +1,23 @@
+package SeratusHariJava;
+
+public class Day61 {
+
+    public static void main(String[] args) {
+        // Infinite Loop
+        
+        // 1. Infinite do while
+//        do {
+//            System.out.println("Menunggu Tiada Akhir");
+//        }while(true);
+        
+        // 2. Infinite while
+//        while(true){
+//            System.out.println("Menunggu Tiada Akhir");
+//        }
+        
+        // 3. Infinite for
+        for (;;) {
+            System.out.println("Menunggu Tiada Akhir");
+        }
+    }
+}
