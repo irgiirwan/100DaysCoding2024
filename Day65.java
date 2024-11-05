b4f3c83522861e58201360c17249f6858ed50dce
A. Irgi Irwan. A <irgiunsulbar24@gmail.com>
2024-11-05 23:05:28 +0800

    Day65.java
    Nested Loop

---
+++ Day65.java
@@ -0,0 +1,14 @@
+package SeratusHariJava;
+
+public class Day65 {
+
+    public static void main(String[] args) {
+        // Nested Loop
+
+        for (int i = 1; i <= 4; i++) {
+            for (int j = 1; j <= 4; j++) {
+                System.out.println("i: "+i+" j: "+j);
+            }
+        }
+    }
+}
