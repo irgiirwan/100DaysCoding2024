777cb3492f4d466581842d02e7732253fbfc750d
A. Irgi Irwan. A <irgiunsulbar24@gmail.com>
2024-11-08 23:49:14 +0800

    Day68.java
    Method Non-void

---
+++ Day68.java
@@ -0,0 +1,15 @@
+package SeratusHariJava;
+
+public class Day68 {
+    // Method Non-void
+    
+    public static int luasPersegi(int sisi) { 
+        int luasPersegi = sisi + sisi; 
+        return luasPersegi; 
+    } 
+    
+    public static void main(String[] args) { 
+        int hasil = luasPersegi(5);
+        System.out.println("Maka, luas persegi adalah: "+ hasil); 
+    } 
+}
