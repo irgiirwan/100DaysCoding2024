07ee3af99fe0c5163f11c31169feaab797eebfe6
A. Irgi Irwan. A <irgiunsulbar24@gmail.com>
2024-11-11 23:11:07 +0800

    Day71.java
    Method Static

---
+++ Day71.java
@@ -0,0 +1,13 @@
+package SeratusHariJava;
+
+public class Day71 {
+    // Method Static  
+
+    static void kali(int a, int b) {
+        System.out.println("Hasil Perkalian "+a+" x "+b+" = "+(a+b));
+    }
+
+    public static void main(String[] args) {
+        Day71.kali(4, 6);
+    }
+}
