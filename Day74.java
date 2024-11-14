757e61b17ef668c8e5f30e3a63f120f8c97c32c6
A. Irgi Irwan. A <irgiunsulbar24@gmail.com>
2024-11-14 22:04:04 +0800

    Day74.java
    String Method : length()

---
+++ Day74.java
@@ -0,0 +1,13 @@
+package SeratusHariJava;
+
+public class Day74 {
+    // String Method : length()
+    
+    public static void main(String[] args) {
+        String kata = "Tunjukkan Sekarang";
+        
+        int panjangKata = kata.length();
+
+        System.out.println("Jadi, panjang dari kata " + kata + " adalah: " + panjangKata);
+    }
+}
