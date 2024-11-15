a5efec3d543ef7561a61f3ed408b21a0a5e855b4
A. Irgi Irwan. A <irgiunsulbar24@gmail.com>
2024-11-15 18:18:57 +0800

    Day75.java
    String Method : toLowerCase()

---
+++ Day75.java
@@ -0,0 +1,14 @@
+package SeratusHariJava;
+
+public class Day75 {
+    // String Method : toLowerCase() 
+    
+    public static void main(String[] args) {
+        String kata = "TunJUkkAn SeKarANg";
+        
+        String teksLower = kata.toLowerCase();
+
+        System.out.println("Teks Asli \""+kata+"\".");
+        System.out.println("Teks setelah konversi menggunakan toLowerCase \""+teksLower+"\".");
+    }
+}
