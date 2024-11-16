209fdc06b78ec73036cbb53dfb951aa6c349f839
A. Irgi Irwan. A <irgiunsulbar24@gmail.com>
2024-11-16 22:51:06 +0800

    Day76.java
    String Method : toUpperCase()

---
+++ Day76.java
@@ -0,0 +1,14 @@
+package SeratusHariJava;
+
+public class Day76 {
+    // String Method : toUpperCase() 
+    
+    public static void main(String[] args) {
+        String kata = "TunJUkkAn SeKarANg";
+        
+        String teksUpper = kata.toUpperCase();
+
+        System.out.println("Teks Asli \""+kata+"\".");
+        System.out.println("Teks setelah konversi menggunakan toUpperCase \""+teksUpper+"\".");
+    }
+}
