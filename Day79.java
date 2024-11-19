e4a64e560045518a20b6b1b64f46d7a9888213d1
A. Irgi Irwan. A <irgiunsulbar24@gmail.com>
2024-11-19 23:24:48 +0800

    Day79.java
    String Method : toString()

---
+++ Day79.java
@@ -0,0 +1,15 @@
+package SeratusHariJava;
+    
+public class Day79 {
+    // String Method : toString()
+           
+    public static void main(String[] args) {
+        String nama = "A. Irgi Irwan. A";
+        int noRegistrasi = 9729473;
+        
+        String StrRegistrasi = Integer.toString(noRegistrasi);
+        
+        System.out.printf("Atas Nama %s dengan No. Registrasi %s",nama,StrRegistrasi);
+        System.out.println("");
+    }
+}
