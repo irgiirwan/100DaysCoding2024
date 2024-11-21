658160c35cb5d1fdb82a783271aa0faa1f71c1e0
A. Irgi Irwan. A <irgiunsulbar24@gmail.com>
2024-11-21 22:58:33 +0800

    Day81.java
    String Method : valueOf()

---
+++ Day81.java
@@ -0,0 +1,12 @@
+package SeratusHariJava;
+
+public class Day81 {
+    public static void main(String[] args) {
+        // String Method : valueOf()
+        
+        char code = 'D';
+        char code1 = '0';
+        int codeAngka = 224318;
+        System.out.print("NIM. "+String.valueOf(code)+""+String.valueOf(code1)+""+String.valueOf(codeAngka));
+    }
+}
