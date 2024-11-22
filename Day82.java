19a4ff3115ce922253ec5fe16c04c7c457f13d9a
A. Irgi Irwan. A <irgiunsulbar24@gmail.com>
2024-11-22 22:18:58 +0800

    Day82.java
    Method ceil(), floor() dan round()

---
+++ Day82.java
@@ -0,0 +1,11 @@
+package SeratusHariJava;
+
+public class Day82 {
+    public static void main(String[] args) {
+        // Method ceil(), floor() dan round()
+        
+        System.out.println("ceil(3.2): " + Math.ceil(3.2));
+        System.out.println("floor(3.8): " + Math.floor(3.8));
+        System.out.println("round(3.5): " + Math.round(3.5));
+    }
+}
