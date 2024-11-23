d30fcd93bccde9845c5c2135e8851af36d291ddf
A. Irgi Irwan. A <irgiunsulbar24@gmail.com>
2024-11-23 22:16:05 +0800

    Day83.java
    Membuat Objek Array satu dimensi

---
+++ Day83.java
@@ -0,0 +1,23 @@
+package SeratusHariJava;
+
+public class Day83 {
+    public static void main(String[] args) {
+        // Membuat Objek Array satu dimensi 
+        
+        //cara pertama
+        //int angka[] = new int[5];
+        
+        //cara kedua
+        //int angka [] = new int[5];
+        
+        //cara ketiga
+        //int[] angka = new int[5];
+        
+        //cara keempat
+        //int [] angka = new int[5];
+        
+        //cara kelima
+        int[] angka;
+        angka = new int[5];
+    }
+}
