36430fcb4ac936d38ba855a32b988246d8918444
A. Irgi Irwan. A <irgiunsulbar24@gmail.com>
2024-11-25 13:20:42 +0800

    Day85.java
    Mengisi elemen ke Array satu dimensi pada saat di inisialisasi

---
+++ Day85.java
@@ -0,0 +1,22 @@
+package SeratusHariJava;
+
+public class Day85 {
+    public static void main(String[] args) {
+        // Mengisi elemen ke Array satu dimensi pada saat di inisialisasi 
+        
+        // cara pertama
+        // int angka[] = new int[]{1,2,3,4,5};
+        
+        // cara kedua
+        // int angka[] = {1,2,3,4,5};
+        
+        // cara ketiga
+        int angka[] = new int[5];
+        angka[0] = 1;
+        angka[1] = 2;
+        angka[2] = 3;
+        angka[3] = 4;
+        angka[4] = 5;
+        
+    }
+}
